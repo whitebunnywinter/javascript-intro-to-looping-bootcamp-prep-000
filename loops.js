830eaf90d640af96bf44c7d14c338c441a33d08a
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-21 02:01:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,6 +12,10 @@ function whileLoop(n) {
   return 'done'
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function doWhileLoop(array) {
   do {
     
